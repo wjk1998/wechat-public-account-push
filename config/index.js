d8d575f502481b94256d770092fc601caf5e7bf5
wjk1998 <113292176+wjk1998@users.noreply.github.com>
2022-09-11 21:33:03 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -108,7 +108,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "9k3r0b7IVsVh30q59KCFRLMY5Limpj77ToehWr7mbOY",
+  CALLBACK_TEMPLATE_ID: "oQLa9iSCIGwRbN-21F6aWvsrSuGLBVRtKQpSBBSRaSI",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
